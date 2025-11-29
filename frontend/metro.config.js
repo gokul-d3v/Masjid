f06e4c7894d47aa5185ff40e5fb4b1374166b2b5
gokul-d3v <richugokulkm@gmail.com>
2025-11-29 13:11:21 +0530

    Update Metro config to work around node:sea issue on Windows

--- frontend/metro.config.js
+++ frontend/metro.config.js
@@ -1,5 +1,12 @@
+// metro.config.js
 const { getDefaultConfig } = require('@expo/metro-config');
 
 const config = getDefaultConfig(__dirname);
+
+// Workaround for the node:sea issue on Windows with Node.js 17+
+config.resolver.unstable_enableSymlinks = false;
+
+// Disable problematic externals that cause the node:sea issue
+config.resolver.nodeModulesExts = ['ts', 'tsx', 'js', 'jsx', 'json'];
 
 module.exports = config;
